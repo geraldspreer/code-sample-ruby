b1305cc3784d31b9c24ba8d1bf253365291e1a72
Gerald Spreer <geraldspreer@gmail.com>
2020-09-30 19:59:34 +0200

    Extract into class

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,23 +1,29 @@
 # frozen_string_literal: true
 
 class FizzBuzz
-  def decide(number)
-    return 'fizzbuzz' if divible_by_five(number) && divible_by_tree(number)
-    return 'fizz' if divible_by_tree(number)
-    return 'buzz' if divible_by_five(number)
+  def decide(input)
+    number = NumberToDecideOn.new(input)
+
+    return 'fizzbuzz' if number.divible_by_five && number.divible_by_tree
+    return 'fizz' if number.divible_by_tree
+    return 'buzz' if number.divible_by_five
 
     # First have this here and fail
     # return 'fizzbuzz' if (number % 5).zero? && (number % 3).zero?
-    number
+    input
+  end
+end
+
+class NumberToDecideOn
+  def initialize(number)
+    @number = number
   end
 
-  private
-
-  def divible_by_tree(number)
-    (number % 3).zero?
+  def divible_by_tree
+    (@number % 3).zero?
   end
 
-  def divible_by_five(number)
-    (number % 5).zero?
+  def divible_by_five
+    (@number % 5).zero?
   end
 end
