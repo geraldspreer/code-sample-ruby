08948f3c27e31c17dfc8b62dc11304e7309d2d05
Gerald Spreer <geraldspreer@gmail.com>
2020-09-30 19:53:03 +0200

    Refactor divisible by methods

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -2,12 +2,22 @@
 
 class FizzBuzz
   def decide(number)
-    return 'fizzbuzz' if (number % 5).zero? && (number % 3).zero?
-    return 'fizz' if (number % 3).zero?
-    return 'buzz' if (number % 5).zero?
+    return 'fizzbuzz' if divible_by_five(number) && divible_by_tree(number)
+    return 'fizz' if divible_by_tree(number)
+    return 'buzz' if divible_by_five(number)
 
     # First have this here and fail
     # return 'fizzbuzz' if (number % 5).zero? && (number % 3).zero?
     number
+  end
+
+  private
+
+  def divible_by_tree(number)
+    (number % 3).zero?
+  end
+
+  def divible_by_five(number)
+    (number % 5).zero?
   end
 end
