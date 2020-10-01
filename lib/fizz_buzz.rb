775e818a22113f08f5f104a01c853396b8b54ee3
Gerald Spreer <g.spreer@neusta.de>
2020-10-01 05:32:45 +0200

    Even shorter

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,20 +1,23 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 
 class FizzBuzz
   def decide(input)
     number = NumberToDecideOn.new(input)
+    answer = ''
+    answer << 'fizz' if number.is.divisible_by 3
+    answer << 'buzz' if number.is.divisible_by 5
 
-    return 'fizzbuzz' if number.divisible_by(3 * 5)
-    return 'fizz' if number.divisible_by 3
-    return 'buzz' if number.divisible_by 5
-
-    input
+    answer.empty? ? input : answer
   end
 end
 
 class NumberToDecideOn
   def initialize(number)
     @number = number
+  end
+
+  def is
+    self
   end
 
   def divisible_by(number)
