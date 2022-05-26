7f888be25f99f47ac066d521fbcb42c73424a42a
Gerald Spreer <geraldspreer@gmail.com>
2022-05-26 04:30:20 +0200

    Clean up code

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,26 +1,30 @@
+#!/usr/bin/ruby
 # frozen_string_literal: false
 
 class FizzBuzz
-  def decide(input)
-    number = NumberToDecideOn.new(input)
+  def ask_about(number_to_check)
+    number = FizzBuzzNumber.new(number_to_check)
     answer = ''
-    answer << 'fizz' if number.is.divisible_by 3
-    answer << 'buzz' if number.is.divisible_by 5
+    answer << 'fizz' if number.divisible_by? 3
+    answer << 'buzz' if number.divisible_by? 5
 
-    answer.empty? ? input : answer
+    answer.empty? ? number_to_check : answer
   end
 end
 
-class NumberToDecideOn
+class FizzBuzzNumber
   def initialize(number)
     @number = number
   end
 
-  def is
-    self
-  end
-
-  def divisible_by(number)
+  def divisible_by?(number)
     (@number % number).zero?
+  end
+end
+
+if $PROGRAM_NAME == __FILE__
+  fizz_buzz = FizzBuzz.new
+  1.upto 100 do |number|
+    puts fizz_buzz.ask_about number
   end
 end
